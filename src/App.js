cc53b880212ab7c2197533905aedb679727844c4
lucasportella <lucasportellaagu@gmail.com>
2021-06-02 15:06:15 -0300

    create route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 function App() {
   return (
-    <>
-      <Login />
-      <Wallet />
-    </>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/wallet" component={ Wallet } />
+    </Switch>
   );
 }
 
