4d5ff18ef190a74e110fe4871b8e7dd1c43327a4
lucasportella <lucasportellaagu@gmail.com>
2021-06-02 14:55:03 -0300

    handle change logic

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,53 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
   componentDidUpdate() {
     if (true) {
       document.getElementById('btn').disabled = false;
     }
   }
 
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
       <form>
         <label htmlFor="id-email">
           Email
-          <input data-testid="email-input" id="id-email" type="email" />
+          <input
+            data-testid="email-input"
+            id="id-email"
+            type="email"
+            value={ email }
+            onChange={ this.handleChange }
+            name="email"
+          />
         </label>
         <label htmlFor="id-senha">
           Senha
-          <input data-testid="password-input" id="id-senha" type="password" />
+          <input
+            data-testid="password-input"
+            id="id-senha"
+            type="password"
+            value={ password }
+            onChange={ this.handleChange }
+            name="password"
+          />
         </label>
         <button type="button" disabled id="btn">Entrar</button>
       </form>
