c475c19a9ef7587145179fc6b4b26ef55b8da69d
lucasportella <lucasportellaagu@gmail.com>
2021-06-02 14:06:38 -0300

    create forms

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <>
+      <Login />
+      <Wallet />
+    </>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label htmlFor="id-email">
+          Email
+          <input data-testid="email-input" id="id-email" type="email" />
+        </label>
+        <label data-testid="password-input" htmlFor="id-senha">
+          Senha
+          <input />
+        </label>
+        <button type="button">Entrar</button>
+      </form>
+    );
   }
 }
 
