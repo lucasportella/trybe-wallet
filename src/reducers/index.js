0a06b63acd4eaca76bb362f244a9b5f81f3adc5c
lucasportella <lucasportellaagu@gmail.com>
2021-06-02 15:23:18 -0300

    compose with devtools

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,14 @@
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import userReducer from './user';
 import walletReducer from './wallet';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
-&& window.__REDUX_DEVTOOLS_EXTENSION__();
-
 const rootReducer = combineReducers({ userReducer, walletReducer });
 
-const store = createStore(rootReducer, devTools);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
